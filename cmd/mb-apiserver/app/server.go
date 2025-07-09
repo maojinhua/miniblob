077b9007a08c89380de4c10f118cc33385de740c
maojinhua <maojinhua@xmfunny.com>
2025-07-09 10:26:02 +0800

    miniBlog 应用配置

--- cmd/mb-apiserver/app/server.go
+++ cmd/mb-apiserver/app/server.go
@@ -1,4 +1,4 @@
-// "Copyright 2025 mjh 【694142812@qq.com】 All rights reserved." | unescape
+// Copyright 2024 孔令飞 <colin404@foxmail.com>. All rights reserved.
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file. The original repo for
 // this file is https://github.com/onexstack/miniblog. The professional
@@ -7,13 +7,18 @@
 package app
 
 import (
-	"fmt"
-
+	"example.com/miniblog/cmd/mb-apiserver/app/options"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var configFile string // 配置文件路径
 
 // NewMiniBlogCommand 创建一个 *cobra.Command 对象，用于启动应用程序.
 func NewMiniBlogCommand() *cobra.Command {
+	// 创建默认的应用命令行选项
+	opts := options.NewServerOptions()
+
 	cmd := &cobra.Command{
 		// 指定命令的名字，该名字会出现在帮助信息中
 		Use: "mb-apiserver",
@@ -48,12 +53,50 @@ The project features include:
 		SilenceUsage: true,
 		// 指定调用 cmd.Execute() 时，执行的 Run 函数
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Println("Hello MiniBlog! HaHa!")
-			return nil
+			return run(opts)
 		},
 		// 设置命令运行时的参数检查，不需要指定命令行参数。例如：./miniblog param1 param2
 		Args: cobra.NoArgs,
 	}
 
+	// 初始化配置函数，在每个命令运行时调用
+	cobra.OnInitialize(onInitialize)
+
+	// cobra 支持持久性标志(PersistentFlag)，该标志可用于它所分配的命令以及该命令下的每个子命令
+	// 推荐使用配置文件来配置应用，便于管理配置项
+	cmd.PersistentFlags().StringVarP(&configFile, "config", "c", filePath(), "Path to the miniblog configuration file.")
+
+	// 将 ServerOptions 中的选项绑定到命令标志
+	opts.AddFlags(cmd.PersistentFlags())
+
 	return cmd
+}
+
+func run(opts *options.ServerOptions) error {
+	// 将 viper 中的配置解析到 opts.
+	if err := viper.Unmarshal(opts); err != nil {
+		return err
+	}
+
+	// 校验命令行选项
+	if err := opts.Validate(); err != nil {
+		return err
+	}
+
+	// 获取应用配置.
+	// 将命令行选项和应用配置分开，可以更加灵活的处理 2 种不同类型的配置.
+	cfg, err := opts.Config()
+	if err != nil {
+		return err
+	}
+
+	// 创建服务器实例.
+	// 注意这里是联合服务器，因为可能同时启动多个不同类型的服务器.
+	server, err := cfg.NewUnionServer()
+	if err != nil {
+		return err
+	}
+
+	// 启动服务器
+	return server.Run()
 }

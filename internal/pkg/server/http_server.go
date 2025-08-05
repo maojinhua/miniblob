27cfb02de51a00db35e7ec3e1810e52a7359ea47
maojinhua <maojinhua@xmfunny.com>
2025-08-05 17:58:43 +0800

    添加优雅关闭

--- internal/pkg/server/http_server.go
+++ internal/pkg/server/http_server.go
@@ -34,6 +34,11 @@ func (s *HTTPServer) RunOrDie() {
 }
 
 // GracefulStop 优雅地关闭 HTTP 服务器.
+// http.Server 类型的 Shutdown 方法的工作流程如下：​​
+// 首先关闭所有已开启的监听器，然后关闭所有空闲连接，最后等待所有活跃连接进入空闲状态后终止服务。
+// 如果传入的 ctx 在服务完成终止之前超时，则 Shutdown 方法会返回与 context 相关的错误。否则会返回由关闭服务监听器引发的其他错误。
+// 当 Shutdown 方法被调用时，Serve、ListenAndServe 以及 ListenAndServeTLS 方法会立即返回 ErrServerClosed 错误。
+// ErrServerClosed 错误被视为服务关闭时的正常行为。因此，如果 ListenAndServe 返回该错误，程序不会打印错误信息。
 func (s *HTTPServer) GracefulStop(ctx context.Context) {
 	log.Infow("Gracefully stop HTTP(s) server")
 	if err := s.srv.Shutdown(ctx); err != nil {

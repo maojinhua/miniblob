00dd44cf8172d7507c64a63a190814c676cde209
maojinhua <maojinhua@xmfunny.com>
2025-10-14 11:33:31 +0800

    29 项目进行性能分析

---
+++ examples/performance/runtimepprof/main.go
@@ -0,0 +1,52 @@
+// Copyright 2024 孔令飞 <colin404@foxmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://github.com/onexstack/miniblog. The professional
+// version of this repository is https://github.com/onexstack/onex.
+
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"runtime"
+	"runtime/pprof"
+)
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+
+// 执行 go run main.go -cpuprofile=cpu.profile -memprofile=mem.profile 生成 cpu.profile 和 mem.profile 文件
+// 然后使用 go tool pprof cpu.profile 查看 CPU 性能分析结果
+// 使用 go tool pprof mem.profile 查看内存性能分析结果
+// 在 pprof 交互界面中，可以使用 top、list、web 等命令查看性能分析结果
+
+func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	// ... rest of the program ...
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		runtime.GC()    // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+	}
+}
